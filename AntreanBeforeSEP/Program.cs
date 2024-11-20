2cef9886a1d82b7f2bb1d2d527dd44dffffb2957
IlhamHrnd <79568527+IlhamHrnd@users.noreply.github.com>
2024-11-20 11:08:00 +0700

    Add project files.

---
+++ AntreanBeforeSEP/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using Serilog;
+
+internal class Program
+{
+    private static readonly string[] AppType = ["MG", "JK", "JS", "WS", "AO"];
+    private static readonly HashSet<string> ServiceUnitIds = ["D1.3.A03", "D2.1.A01", "D2.1.A03", "D2.1.A08"];
+    private static readonly HashSet<int> TaskIds = [1, 2, 3, 4, 5];
+    private static int Minutes = 60;
+    private static void Main(string[] args)
+    {
+        Log.Logger = new LoggerConfiguration()
+            .WriteTo.File("logs/log.txt", rollingInterval: RollingInterval.Day)
+            .CreateLogger();
+
+        IConfiguration config = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .AddEnvironmentVariables()
+            .Build();
+    }
+}
